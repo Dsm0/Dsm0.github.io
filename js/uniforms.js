54dbb4032fe46d3d9d79d96efb569ee155fed4c4
Dsm0 <willrinkoff@gmail.com>
2020-05-13 17:24:27 -0500

    added u_left

--- js/uniforms.js
+++ js/uniforms.js
@@ -1,4 +1,7 @@
-const uniformDict = {"u_top": u_topGet}
+const uniformDict = {
+    "u_top": u_topGet,
+    "u_left": u_leftGet
+}
 
 function genUniforms(shaderObj){
     const uniformList = shaderObj.uniformList;
@@ -19,6 +22,19 @@ function u_topGet(shaderObj){
       description: "position of canvas relative to top of screen",
       getLocation: function(){return shaderObj.gl.getUniformLocation(shaderObj.program, "u_top")},
       getValue: function(){return parseFloat(shaderObj.canvas.style.top)}, //these two functions are for debugging puroses more than anything
+      type: "1f"
+    }
+    uniform.genValue = function(){shaderObj.gl.uniform1f(uniform.getLocation(),uniform.getValue())};
+    shaderObj.uniforms.push(uniform);
+}
+
+
+function u_leftGet(shaderObj){
+    const uniform = {
+      name: "u_left",
+      description: "position of canvas relative to left edge of screen",
+      getLocation: function(){return shaderObj.gl.getUniformLocation(shaderObj.program, "u_left")},
+      getValue: function(){return parseFloat(shaderObj.canvas.style.left)}, //these two functions are for debugging puroses more than anything
       type: "1f"
     }
     uniform.genValue = function(){shaderObj.gl.uniform1f(uniform.getLocation(),uniform.getValue())};
