024c4b308e9c9dc821afe0585cf3719d587a35b9
Dsm0 <willrinkoff@gmail.com>
2020-05-14 16:13:00 -0500

    added static version of resume to page

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,12 @@ function seconds_elapsed ()
   return ( seconds_elapsed ); 
 }
 
+// courtesy of
+// https://stackoverflow.com/a/9976309 
+function resizeIframe(obj) {
+  obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+}
+
 function move(moveme, x_pos, y_pos) {
     moveme.style.left = x_pos+'px';
     moveme.style.top = y_pos+'px';
