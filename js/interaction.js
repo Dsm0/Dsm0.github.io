de7350b94e366d1a42de3d081205f36799d3a463
Dsm0 <willrinkoff@gmail.com>
2020-05-14 17:08:33 -0500

    reordered how html+js are loaded

--- js/interaction.js
+++ js/interaction.js
@@ -2,17 +2,23 @@ var centerX = window.innerWidth/2;
 var centerY = window.innerHeight/2;
 var static = false;
 
-window.onload = function(e){
+// window.onload = function(e)
+var initScripts = function(e){
 
    var info = document.getElementById("contact");
    var canvas1 = document.getElementById("canvas1");
    var canvas2 = document.getElementById("canvas2");
    var email = document.getElementById("email");
+   var resume = document.getElementById("resume");
 
    move(info,centerX,centerY);
    move(canvas1,centerX + window.innerWidth,centerY);
    move(canvas2,centerX + window.innerWidth,centerY+100);
    move(email,centerX + 100,centerY + 100);
+
+   // moveElemByMouse(e,"resume",centerX*1.2,centerY+800);
+
+   resizeIframe(resume);
 }
 
 var onkeypress = function(e){
@@ -34,6 +40,9 @@ var onmousemove = function(e){
       moveElemByMouse(e,"canvas1",centerX*1.4,centerY,xfactor=2,yfactor=1);
       moveElemByMouse(e,"canvas2",centerX*1.4,centerY+100,xfactor=2,yfactor=2);
       moveElemByMouse(e,"email",pivotX=centerX+70,pivotY=centerY+70,yfactor=0.7);
+
+      // moveElemByMouse(e,"resume",centerX*1.2,centerY+800);
+
   }
 }
 
