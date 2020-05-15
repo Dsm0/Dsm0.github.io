a74bf40df4c248640e34034a6840b89c4f136c9a
Dsm0 <willrinkoff@gmail.com>
2020-05-15 12:36:18 -0500

    revamped static version of site

--- js/interaction.js
+++ js/interaction.js
@@ -23,20 +23,75 @@ var initScripts = function(e){
    //resizeIframe(resume);
 }
 
-var onkeypress = function(e){
-   console.log(e);
+function makeStatic(elem){
+   var newWidth = window.innerWidth  * (parseFloat(elem.getAttribute("data-staticX"))/100);
+   var newHeight = window.innerHeight * (parseFloat(elem.getAttribute("data-staticY"))/100);
+   // "data-staticX" and "data-staticY" are stored as percent values
+   // but I convert them to pixels so that the uniforms passed to shaders don't become 0-100
+   move(elem,newWidth,newHeight);
+}
 
-   // if(e.key == "s"){
-   //    static = !static;
-   //    if(static){
-   //       changeCSS("css/static.css");
-   //    } else {
-   //       changeCSS("css/dynamic.css");
-   //    }
-   // }
+
+// It won't scale, but it's good for now...
+function staticify(){
+
+   var info = document.getElementById("contact");
+   var canvas1 = document.getElementById("canvas1");
+   var canvas2 = document.getElementById("canvas2");
+   var email = document.getElementById("email");
+   var resume = document.getElementById("resume");
+
+   info.style.transition = "0.2s";
+   canvas1.style.transition = "0.2s";
+   canvas2.style.transition = "0.2s";
+   email.style.transition = "0.2s";
+   resume.style.transition = "0.2s";
+
+
+   // move(canvas1,canvas1.data-staticX,canvas1.data-staticY);
+   makeStatic(canvas1);
+   makeStatic(canvas2);
+
+   // canvas2.style.left = canvas1.getAttribute("data-staticX")
+   // canvas2.style.top= canvas1.getAttribute("data-staticY")
+   // move(canvas2,centerX*1.3,centerY*1.5);
+
+   move(info,centerX*0.85,centerY*1.6);
+   move(email,centerX*0.87,centerY*1.8);
+   move(resume,centerX*0.03,centerY*0.3);
+}
+
+
+// It won't scale, but it's good for now...
+function dynamify(){
+
+   var info = document.getElementById("contact");
+   var canvas1 = document.getElementById("canvas1");
+   var canvas2 = document.getElementById("canvas2");
+   var email = document.getElementById("email");
+   var resume = document.getElementById("resume");
+
+   info.style.transition = "0s";
+   canvas1.style.transition = "0s";
+   canvas2.style.transition = "0s";
+   email.style.transition = "0s";
+   resume.style.transition = "0s";
 
 }
 
+
+
+var onkeypress = function(e){
+   console.log(e);
+   if(e.key == "s"){
+      static = !static;
+      if(static){
+         staticify();
+      } else {
+         dynamify();
+      }
+   }  
+}
 
 var onmousemove = function(e){
    if(!static){
@@ -45,18 +100,12 @@ var onmousemove = function(e){
       moveElemByMouse(e,"canvas2",centerX*1.4,centerY+100,xfactor=2,yfactor=2);
       moveElemByMouse(e,"email",pivotX=centerX+70,pivotY=centerY+70,yfactor=0.7);
       moveElemByMouseMax(e,"resume",centerX*0.3,centerY*0.3,xfactor=4,yfactor=0,maxX=centerX*0.1);
-  } else {
-      changeCSS("css/static.css");
-  }
+      }
 }
 
 function changeCSS(cssFile) {
    var cssLoader = document.getElementById("cssLoader");
    cssLoader.href = cssFile;
-}
-
-function debug(){
-   console.log("over");
 }
 
 // ^^^^^^^^^^^^^
