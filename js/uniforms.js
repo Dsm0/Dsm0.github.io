22081aaafcb54a8f36a068bcdd534af8b835a7ea
Dsm0 <willrinkoff@gmail.com>
2020-05-16 13:55:12 -0500

    changed how positions are passed to uniforms

--- js/uniforms.js
+++ js/uniforms.js
@@ -1,6 +1,7 @@
 const uniformDict = {
-    "u_top": u_topGet,
-    "u_left": u_leftGet,
+    //"u_top": u_topGet,
+    //"u_left": u_leftGet,
+    "u_pos": u_pagePositionGet,
     "u_window_resolution":u_window_resolutionGet,
     "u_canvas_resolution":u_canvas_resolutionGet
 }
@@ -18,30 +19,43 @@ function genUniforms(shaderObj){
 }
 
 
-function u_topGet(shaderObj){
+//function u_topGet(shaderObj){
+    //const uniform = {
+      //name: "u_top",
+      //description: "position of canvas relative to top of screen",
+      //getLocation: function(){return shaderObj.gl.getUniformLocation(shaderObj.program, "u_top")},
+      //getValue: function(){return parseFloat(shaderObj.canvas.style.top)}, //these two functions are for debugging puroses more than anything
+      //type: "1f"
+    //}
+    //uniform.genValue = function(){shaderObj.gl.uniform1f(uniform.getLocation(),uniform.getValue())};
+    //shaderObj.uniforms.push(uniform);
+//}
+
+
+//function u_leftGet(shaderObj){
+    //const uniform = {
+      //name: "u_left",
+      //description: "position of canvas relative to left edge of screen",
+      //getLocation: function(){return shaderObj.gl.getUniformLocation(shaderObj.program, "u_left")},
+      //getValue: function(){return parseFloat(shaderObj.canvas.style.left)}, //these two functions are for debugging puroses more than anything
+      //type: "1f"
+    //}
+    //uniform.genValue = function(){shaderObj.gl.uniform1f(uniform.getLocation(),uniform.getValue())};
+    //shaderObj.uniforms.push(uniform);
+//}
+
+function u_pagePositionGet(shaderObj){
     const uniform = {
-      name: "u_top",
-      description: "position of canvas relative to top of screen",
-      getLocation: function(){return shaderObj.gl.getUniformLocation(shaderObj.program, "u_top")},
-      getValue: function(){return parseFloat(shaderObj.canvas.style.top)}, //these two functions are for debugging puroses more than anything
-      type: "1f"
+      name: "u_pos",
+      description: "position of canvas relative to inner screen width and height",
+      getLocation: function(){return shaderObj.gl.getUniformLocation(shaderObj.program, "u_pos")},
+      getValue: function(){return [parseFloat(shaderObj.canvas.style.left),parseFloat(shaderObj.canvas.style.top)]}, //these two functions are for debugging puroses more than anything
+      type: "2f"
     }
-    uniform.genValue = function(){shaderObj.gl.uniform1f(uniform.getLocation(),uniform.getValue())};
+    uniform.genValue = function(){var pos = uniform.getValue(); shaderObj.gl.uniform2f(uniform.getLocation(),pos[0],pos[1])};
     shaderObj.uniforms.push(uniform);
 }
 
-
-function u_leftGet(shaderObj){
-    const uniform = {
-      name: "u_left",
-      description: "position of canvas relative to left edge of screen",
-      getLocation: function(){return shaderObj.gl.getUniformLocation(shaderObj.program, "u_left")},
-      getValue: function(){return parseFloat(shaderObj.canvas.style.left)}, //these two functions are for debugging puroses more than anything
-      type: "1f"
-    }
-    uniform.genValue = function(){shaderObj.gl.uniform1f(uniform.getLocation(),uniform.getValue())};
-    shaderObj.uniforms.push(uniform);
-}
 
 
 function u_window_resolutionGet(shaderObj){
@@ -72,3 +86,5 @@ function u_canvas_resolutionGet(shaderObj){
         shaderObj.gl.uniform2f(uniform.getLocation(), values[0],values[1])};
     shaderObj.uniforms.push(uniform);
 }
+
+
