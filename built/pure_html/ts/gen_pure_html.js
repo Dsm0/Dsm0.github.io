07da47edff59d98f942c01edc58785091a313f1e
Dsm0 <willrinkoff@gmail.com>
2020-08-13 16:49:17 -0500

    Squashed commit of the following:
    commit 06aaf29a8c662d1edabcc59ca1cfd52ebddde920
    Merge: 011b6a3 816e4ae
    Author: Dsm0 <willrinkoff@gmail.com>
    Date:   Thu Aug 13 16:46:02 2020 -0500
    
        Merge branch 'typescript' of github.com:Dsm0/Dsm0.github.io into typescript
    
    commit 011b6a31d0f8cf373ff8fbf27a4f8e1089ef3a2e
    Author: Dsm0 <willrinkoff@gmail.com>
    Date:   Thu Aug 13 16:45:59 2020 -0500
    
        made code a tad slicker
    
    commit 816e4aecbb4bc3cf6e7a0e4773d3ca8dc753166a
    Author: Dsm0 <willrinkoff@gmail.com>
    Date:   Thu Aug 13 16:45:39 2020 -0500
    
        made code a tad slicker
    
    commit 06af252241f5683a4143e9939d7d7114a01442ae
    Author: Dsm0 <willrinkoff@gmail.com>
    Date:   Thu Aug 13 16:43:20 2020 -0500
    
        made code slick

--- built/pure_html/ts/gen_pure_html.js
+++ built/pure_html/ts/gen_pure_html.js
@@ -7,32 +7,37 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const doc_ = document;
 function build() {
     return __awaiter(this, void 0, void 0, function* () {
-        const info_json = yield (yield fetch("/info.json")).json();
-        const body = doc_ === null || doc_ === void 0 ? void 0 : doc_.getElementById("body");
-        if (doc_ && body) {
-            body.insertAdjacentHTML("beforeend", `<div id="top"> ${gen_html("", info_json)}</div>`);
-        }
+        const doc = document, info = yield (yield fetch("/info.json")).json(), body = doc === null || doc === void 0 ? void 0 : doc.getElementById("body");
+        doc && body
+            ? body.insertAdjacentHTML("beforeend", `<div id="top"> 
+                                              ${gen_html("", info)}
+                                            </div>`)
+            : {};
     });
 }
-let depth = 0;
 const key2show = (key, index) => parseInt(key) === index ? "" : key;
-function gen_html(html, o) {
-    let _html = "", isObj = false;
-    Object.keys(o).forEach((k, i) => {
-        const val = o[k], dk = key2show(k, i); // dk = displayed key
+function gen_html(html, obj) {
+    let _html = "";
+    Object.keys(obj).forEach((key, i) => {
+        const val = obj[key], disp_key = key2show(key, i); // dk = displayed key
         switch (typeof val) {
             case "string":
-                _html += `
-        <a class="item ${dk}" ${dk === "link" ? `href=${val}` : ""}">${dk} - ${val}</a>`;
+                _html += `<a class="item ${disp_key}" 
+                     ${disp_key === "link" ? `href=${val}` : ""}">
+                    ${disp_key} - ${val}
+                  </a>`;
                 break;
             case "object":
                 _html += `<div class="container">
-        <p class="title ${dk}">${dk}</p>
-           <div class="${dk}">${gen_html(_html, val)}</div>
-           </div>`;
+                    <p class="title ${disp_key}">
+                      ${disp_key}
+                    </p>
+                    <div class="${disp_key}">
+                      ${gen_html(_html, val)}
+                    </div>
+                  </div>`;
                 break;
         }
     });
--- src/pure_html/ts/gen_pure_html.ts
+++ src/pure_html/ts/gen_pure_html.ts
@@ -1,43 +1,38 @@
-const doc_ = document;
-
 async function build() {
-  const info_json = await (await fetch("/info.json")).json();
-
-  const body = doc_?.getElementById("body");
-
-  if (doc_ && body) {
-    body.insertAdjacentHTML(
-      "beforeend",
-      `<div id="top"> ${gen_html("", info_json)}</div>`
-    );
-  }
+  const doc = document
+      , info = await (await fetch("/info.json")).json()
+      , body = doc?.getElementById("body")
+  doc && body 
+    ? body.insertAdjacentHTML("beforeend", `<div id="top"> 
+                                              ${gen_html("", info)}
+                                            </div>`) 
+    : {} 
 }
 
-let depth = 0;
+const key2show = (key: string, index: number) => parseInt(key) === index ? "" : key
 
-const key2show = (key: string, index: number) =>
-  parseInt(key) === index ? "" : key;
-
-function gen_html(html, o: object) {
-  let _html = "",
-    isObj = false;
-  Object.keys(o).forEach((k: string, i: number) => {
-    const val = o[k],
-      dk = key2show(k, i); // dk = displayed key
+function gen_html(html, obj: object) {
+  let _html = ""
+  Object.keys(obj).forEach((key: string, i: number) => {
+    const val = obj[key], disp_key = key2show(key, i) // dk = displayed key
     switch (typeof val) {
       case "string":
-        _html += `
-        <a class="item ${dk}" ${
-          dk === "link" ? `href=${val}` : ""
-        }">${dk} - ${val}</a>`;
-        break;
+        _html += `<a class="item ${disp_key}" 
+                     ${disp_key === "link" ? `href=${val}` : ""}">
+                    ${disp_key} - ${val}
+                  </a>`
+        break
       case "object":
         _html += `<div class="container">
-        <p class="title ${dk}">${dk}</p>
-           <div class="${dk}">${gen_html(_html, val)}</div>
-           </div>`;
-        break;
+                    <p class="title ${disp_key}">
+                      ${disp_key}
+                    </p>
+                    <div class="${disp_key}">
+                      ${gen_html(_html, val)}
+                    </div>
+                  </div>`
+        break
     }
-  });
-  return _html;
+  })
+  return _html
 }
