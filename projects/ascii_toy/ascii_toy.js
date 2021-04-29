2e3af62715efbec1ebd887e369a17155095fd1f8
Dsm0 <willrinkoff@gmail.com>
2021-04-29 12:53:46 -0400

    added some font features to ascii_toy

--- projects/ascii_toy/ascii_toy.js
+++ projects/ascii_toy/ascii_toy.js
@@ -7,60 +7,64 @@ const start = date.getMilliseconds();
 
 let time = 0.1
 
-const width  = 50 //159
+const width = 50 //159
 const height = 20
 
 let line_length = 0
 
-Array.prototype.replaceAt = function(index, replacement) {
+Array.prototype.replaceAt = function (index, replacement) {
     replacement ||= "";
     return this.subarray(0, index) + replacement + this.subarray(index + replacement.length);
 }
 
-String.prototype.replaceAt = function(index, replacement) {
+String.prototype.replaceAt = function (index, replacement) {
     replacement ||= "";
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
 
-render.innerHTML = [...Array(width*height).keys()].map(i =>{return "<span id='span"+i+"'></span>"}).join("")
+render.innerHTML = [...Array(width * height).keys()].map(i => { return "<span id='span" + i + "'></span>" }).join("")
 
 spans = document.getElementsByTagName("span")
 
 
-function parse_char(ah){
-    switch(ah){
-    case "b":
-        return "bold";
-    case "i":
-        return "italic";
-    case "v":
-        return "invert";
-    case "c":
-        return "cream";
-    case "s":
-        return "shadow";
-    case "1":
-        return "size1";
-    case "2":
-        return "size2";
-    case "3":
-        return "size3";
-    case "4":
-        return "size2";
-    case "5":
-        return "size5";
-    case "6":
-        return "size6";
-    case "7":
-        return "size7";
-    case "8":
-        return "size8";
-    case "9":
-        return "size9";
-    case "r":
-        return "border";
-    default:
-        return "";
+function parse_char(ah) {
+    switch (ah) {
+        case "b":
+            return "bold";
+        case "i":
+            return "italic";
+        case "v":
+            return "invert";
+        case "c":
+            return "cream";
+        case "s":
+            return "shadow";
+        case "&":
+            return "comicsands";
+        case "h":
+            return "hallucination";
+        case "1":
+            return "size1";
+        case "2":
+            return "size2";
+        case "3":
+            return "size3";
+        case "4":
+            return "size2";
+        case "5":
+            return "size5";
+        case "6":
+            return "size6";
+        case "7":
+            return "size7";
+        case "8":
+            return "size8";
+        case "9":
+            return "size9";
+        case "r":
+            return "border";
+        default:
+            return "";
     }
 }
 
@@ -75,27 +79,25 @@ let newline = false
 update_render = () => {
     text = ascii_input.value;
     text_len = text.length;
-    if(text){
-        if((index % width) == width-1 && index>0){
+    if (text) {
+        if ((index % width) == width - 1 && index > 0) {
             spans[index].innerText = "\n"
         } else {
-            let ah = text[char_index%text_len];
-            if(c = parse_char(ah)) {
+            let ah = text[char_index % text_len];
+            if (c = parse_char(ah)) {
                 classes.add(c);
             }
-            if(ah === "\\"){
+            if (ah === "\\" || ah === " ") {
                 classes.clear();
-				ah = " ";
-			}
+                ah = " ";
+            }
             spans[index].className = Array.from(classes).join(' ');
             ah ||= " ";
             spans[index].innerText = ah;
-            if(!c)
-                classes.clear();
-            char_index = (char_index+1) % text_len;
+            char_index = (char_index + 1) % text_len;
         }
-        index = (index+1) % (width*height);
+        index = (index + 1) % (width * height);
     }
 }
 
-let interval = setInterval(update_render,time);
+let interval = setInterval(update_render, time);
