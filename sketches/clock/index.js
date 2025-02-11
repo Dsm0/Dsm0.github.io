6ff579d612a7d6c67d61f1a4e1fb4997685e7002
Dsm0 <willrinkoff@gmail.com>
2025-02-10 19:49:54 -0800

    faust works

--- sketches/clock/index.js
+++ sketches/clock/index.js
@@ -12,10 +12,11 @@ let faustNode;
 const AudioCtx = window.AudioContext || window.webkitAudioContext;
 const audioContext = new AudioCtx({ latencyHint: 0.00001 });
 
+let play = null;
+
 // Activate AudioContext and Sensors on user interaction
 let sensorHandlersBound = false;
 const activateFaustNode = async () => {
-    console.log("AHHHHHHHH");
     
     // Import the requestPermissions function
     const { requestPermissions } = await import("./create-node.js");
@@ -26,20 +27,24 @@ const activateFaustNode = async () => {
     // Activate sensor listeners
     if (!sensorHandlersBound) {
         await faustNode.startSensors();
+        console.log("started sensors");
         sensorHandlersBound = true;
     }
 
-    // Activate or suspend the AudioContext
-    if (audioContext.state === "running") {
-        // $buttonDsp.textContent = "Suspended";
-        await audioContext.suspend();
-    } else if (audioContext.state === "suspended") {
-        // $buttonDsp.textContent = "Running";
-        await audioContext.resume();
-        if (FAUST_DSP_VOICES) {
-            play(faustNode);
-        }
-    }
+    play(faustNode);
+
+    // // Activate or suspend the AudioContext
+    // if (audioContext.state === "running") {
+    //     // $buttonDsp.textContent = "Suspended";
+    //     await audioContext.suspend();
+    // } else if (audioContext.state === "suspended") {
+    //     // $buttonDsp.textContent = "Running";
+    //     await audioContext.resume();
+    //     if (FAUST_DSP_VOICES) {
+    //         play(faustNode);
+    //     }
+    // }
+
 }
 
 // Called at load time
@@ -47,12 +52,13 @@ const activateFaustNode = async () => {
 
     const { createFaustNode, connectToAudioInput } = await import("./create-node.js");
 
-    const play = (node) => {
+    play = (node) => {
+        console.log("play", node);
         node.keyOn(0, 60, 100);
-        setTimeout(() => node.keyOn(0, 64, 100), 1000);
-        setTimeout(() => node.keyOn(0, 67, 100), 2000);
-        setTimeout(() => node.allNotesOff(), 5000);
-        setTimeout(() => play(node), 7000);
+        // setTimeout(() => node.keyOn(0, 64, 100), 1000);
+        // setTimeout(() => node.keyOn(0, 67, 100), 2000);
+        // setTimeout(() => node.allNotesOff(), 5000);
+        // setTimeout(() => play(node), 7000);
     }
 
     // Create Faust node
@@ -60,18 +66,22 @@ const activateFaustNode = async () => {
     faustNode = result.faustNode;  // Assign to the global variable
     if (!faustNode) throw new Error("Faust DSP not compiled");
 
-    // Connect the Faust node to the audio output
-    faustNode.connect(audioContext.destination);
+    // // Connect the Faust node to the audio output
+    // faustNode.connect(audioContext.destination);
 
     // Connect the Faust node to the audio input
     if (faustNode.getNumInputs() > 0) {
         await connectToAudioInput(audioContext, null, faustNode, null);
     }
 
+    console.log("hhhhhhhhhhhhhhhhhhhh", faustNode);
+    
+
     // Create Faust node activation button
     // $buttonDsp.disabled = false;
 
     // Set page title to the DSP name
     document.title = name;
+    
 
 })();
